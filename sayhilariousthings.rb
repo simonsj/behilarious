8d3a1c2fa320ea8faba88220c898bd96e612c55b
Jon Simons <jon@jonsimons.org>
2013-03-02 22:38:52 -0800

    sayhilariousthings.rb: log twiddles

--- sayhilariousthings.rb
+++ sayhilariousthings.rb
@@ -5,25 +5,28 @@
 # Jon Simons
 # March 2, 2013
 
+require 'time'
+
+def Log(s)
+  puts Time.now.utc.iso8601 + " " + s
+  STDOUT.flush
+end
+
 gettext = File.join(File.dirname($0), "getRandomWiki")
-puts("gettext: " + gettext)
+#Log("gettext: " + gettext)
 
 #voiceout = `say -v ? | awk -F"en_US" '{print $1}' | sed -e 's?  *$??g'`
 #voices = voiceout.split(/\n/)
 
 while 1 do
   #voice = voices.choice
-  #puts("Voice: " + voice)
+  #Log("Voice: " + voice)
 
   text = `#{gettext}`
-  puts("Text: " + text)
-  STDOUT.flush
+  Log("Text: " + text)
 
    #`say -v "#{voice}" "#{text}"`
    `say "#{text}"`
 
-  puts("Sleeping now...")
-  STDOUT.flush
-
-  sleep 5
+  sleep (60 * 8)
 end
